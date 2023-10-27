ad2d8efed9bdf15ac86f24e58fe8b12ccc63dde2
MARUAN IBRAHIM MOUSSA <maruan.moussa@alunos.sc.senac.br>
2023-10-27 20:50:55 -0300

    add style

--- hub_trade/lib/pages/loginPage.dart
+++ hub_trade/lib/pages/loginPage.dart
@@ -23,80 +23,97 @@ class _LoginPageState extends State<LoginPage> {
         centerTitle: true,
       ),
       body: Column(
-    children: [
-      Expanded(
-        child: Container(
-          color: Colors.white,
-          padding: EdgeInsets.all(40),
-          child: Column(
-            children: [
-              TextField(
-                decoration: InputDecoration(hintText: 'Login', labelText: 'Login'),
-                controller: input,
-              ),
-              TextField(
-                decoration: InputDecoration(labelText: 'Senha'),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 10, left: 25, right: 25),
-                width: 100,
-                height: 50,
-                decoration: BoxDecoration(
-                  color: Colors.orangeAccent,
-                  borderRadius: BorderRadius.all(Radius.circular(32)),
-                ),
-                child: TextButton(
-                  child: Center(
-                    child: Text(
-                      'Entrar',
-                      style: TextStyle(color: Colors.white),
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.white,
+              padding: EdgeInsets.all(40),
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Login',
+                      hintText: 'Digite seu login',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(
+                            10.0), 
+                      ),
+                      contentPadding: EdgeInsets.all(
+                          15.0), 
+                    ),
+                    controller: input,
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Senha',
+                      hintText: 'Digite sua senha',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      contentPadding: EdgeInsets.all(15.0),
+                    ),
+                    controller: input2,
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 10, left: 25, right: 25),
+                    width: 100,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.orangeAccent,
+                      borderRadius: BorderRadius.all(Radius.circular(32)),
+                    ),
+                    child: TextButton(
+                      child: Center(
+                        child: Text(
+                          'Entrar',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      onPressed: () {
+                        pageController.animateToPage(1,
+                            duration: Duration(microseconds: 200),
+                            curve: Curves.ease);
+                      },
                     ),
                   ),
-                  onPressed: () {
-                    pageController.animateToPage(1,
-                        duration: Duration(microseconds: 200),
-                        curve: Curves.ease);
-                  },
-                ),
+                ],
               ),
-            ],
+            ),
           ),
-        ),
-      ),
-    
-      Positioned(
-        top: 300,
-        child: Container(
-          height: 60,
-          child: BottomNavigationBar(
-            items: [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.facebook),
-                label: 'Facebook',
+          Positioned(
+            top: 300,
+            child: Container(
+              height: 60,
+              child: BottomNavigationBar(
+                items: [
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.facebook),
+                    label: 'Facebook',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Image.asset('assets/google2.png', width: 25),
+                    label: 'Google',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.telegram),
+                    label: 'Telegram',
+                  ),
+                ],
               ),
-              BottomNavigationBarItem(
-                icon: Image.asset('assets/google2.png', width: 25),
-                label: 'Google',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.telegram),
-                label: 'Telegram',
-              ),
-            ],
+            ),
           ),
-        ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 60,
+              color: Colors.orangeAccent,
+            ),
+          ),
+        ],
       ),
-      Positioned(
-        bottom: 0,
-        left: 0,
-        right: 0,
-        child: Container(
-          height: 60,
-          color: Colors.orangeAccent,
-        ),
-      ),
-    ],
-  ),
-);
+    );
   }
 }
