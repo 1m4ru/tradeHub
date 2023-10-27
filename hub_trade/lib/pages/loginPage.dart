88d964abc8056a3bf767b62012bd2231a09ce448
MARUAN IBRAHIM MOUSSA <maruan.moussa@alunos.sc.senac.br>
2023-10-27 20:45:24 -0300

    refatoring code

--- hub_trade/lib/pages/loginPage.dart
+++ hub_trade/lib/pages/loginPage.dart
@@ -22,71 +22,81 @@ class _LoginPageState extends State<LoginPage> {
         elevation: 30,
         centerTitle: true,
       ),
-      body: PageView(
-        children: [
-          Container(
-            color: Colors.white,
-            padding: EdgeInsets.all(40),
-            child: Column(
-              
-              children: [
-                TextField(
-                  decoration:
-                      InputDecoration(hintText: 'Login', labelText: 'Login'),
-                  controller: input,
+      body: Column(
+    children: [
+      Expanded(
+        child: Container(
+          color: Colors.white,
+          padding: EdgeInsets.all(40),
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(hintText: 'Login', labelText: 'Login'),
+                controller: input,
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Senha'),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10, left: 25, right: 25),
+                width: 100,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.orangeAccent,
+                  borderRadius: BorderRadius.all(Radius.circular(32)),
                 ),
-                TextField(
-                  decoration: InputDecoration(labelText: 'senha'),
-                ),
-                Container(
-                  margin: (EdgeInsets.only(top: 10, left: 25, right: 25)),
-                  width: 100,
-                  height: 50,
-                  decoration: BoxDecoration(
-                      color: Colors.orangeAccent,
-                      borderRadius: BorderRadius.all(Radius.circular(32))),
-                  child: TextButton(
-                    child: Center(
-                      child: Text(
-                        'Entrar',
-                        style: TextStyle(color: Colors.white),
-                      ),
+                child: TextButton(
+                  child: Center(
+                    child: Text(
+                      'Entrar',
+                      style: TextStyle(color: Colors.white),
                     ),
-                    onPressed: () {
-                      pageController.animateToPage(1,
-                          duration: Duration(microseconds: 200),
-                          curve: Curves.ease);
-                    },
                   ),
+                  onPressed: () {
+                    pageController.animateToPage(1,
+                        duration: Duration(microseconds: 200),
+                        curve: Curves.ease);
+                  },
                 ),
-                Container(
-                 margin: EdgeInsets.only(top: 30),
-                  width: MediaQuery.of(context).size.width,
-                  height:60,
-                  child: BottomNavigationBar(
-                    items: [
-                      BottomNavigationBarItem(
-                          icon: Icon(Icons.facebook), label: 'Facebook'),
-                      BottomNavigationBarItem(
-                        icon: Image.asset('assets/google2.png', width: 25),
-                        label: 'Google',
-                      ),
-                      BottomNavigationBarItem(
-                          icon: Icon(Icons.telegram), label: 'Telegram'),
-                    ],
-                  ),
-                ),
-               
-              ],
-            ),
+              ),
+            ],
           ),
-           Positioned(
-                  child: Container(
-                    height: 5,
-                    color: Colors.orangeAccent,
-                  ) ),
-        ],
+        ),
       ),
-    );
+    
+      Positioned(
+        top: 300,
+        child: Container(
+          height: 60,
+          child: BottomNavigationBar(
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.facebook),
+                label: 'Facebook',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset('assets/google2.png', width: 25),
+                label: 'Google',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.telegram),
+                label: 'Telegram',
+              ),
+            ],
+          ),
+        ),
+      ),
+      Positioned(
+        bottom: 0,
+        left: 0,
+        right: 0,
+        child: Container(
+          height: 60,
+          color: Colors.orangeAccent,
+        ),
+      ),
+    ],
+  ),
+);
   }
 }
